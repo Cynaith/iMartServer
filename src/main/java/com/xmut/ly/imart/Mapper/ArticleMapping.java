1a1ac336a91d00b4de1834a006d135fce474340b
Cynaith <894324762@qq.com>
2020-03-11 00:19:43 +0800

    add @Mapper

--- src/main/java/com/xmut/ly/imart/Mapper/ArticleMapping.java
+++ src/main/java/com/xmut/ly/imart/Mapper/ArticleMapping.java
@@ -1,8 +1,10 @@
 package com.xmut.ly.imart.Mapper;
 
 import com.xmut.ly.imart.ResultVo.SecondMainVo;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
  **/
 
 @Repository
+@Mapper
 public interface ArticleMapping {
 
     @Select("select id ,img1 as imageUrl,title as name,price,support as fire from article where kind = #{kind}")
