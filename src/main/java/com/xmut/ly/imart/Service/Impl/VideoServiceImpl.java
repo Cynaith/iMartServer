36376bdd41e1ca57a33f0a0c4af3091313538237
Cynaith <894324762@qq.com>
2020-04-05 16:05:34 +0800

    增加视频输出检索，防止视频为空

--- src/main/java/com/xmut/ly/imart/Service/Impl/VideoServiceImpl.java
+++ src/main/java/com/xmut/ly/imart/Service/Impl/VideoServiceImpl.java
@@ -8,10 +8,12 @@ import com.xmut.ly.imart.Service.VideoService;
 import org.apache.ibatis.binding.BindingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.tools.tree.NullExpression;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @USER: lynn
@@ -20,41 +22,57 @@ import java.util.List;
 @Service
 public class VideoServiceImpl implements VideoService {
 
-@Autowired
+    @Autowired
     VideoMapping videoMapping;
-@Autowired
+    @Autowired
     UserMapping userMapping;
 
     @Override
     public List<VideoListVo> getVideos(String username) {
-        List<VideoListVo> videoListVos = videoMapping.getVideos();
-        videoListVos.forEach(videoListVo -> {
-            String id = userMapping.getIdByUsername(username)+"-"+videoListVo.getId();
-            try{
+        List<VideoListVo> videoListVoList = videoMapping.getVideos();
+//        videoListVoList.forEach(videoListVo -> {
+//            try{
+//                if (videoListVo.getVideourl() != null) {
+//                    videoListVos.add(videoListVo);
+//                }
+//            }catch (NullPointerException e){
+//
+//            }
+//
+//        });
+        List<VideoListVo> filterList = null;
+        try{
+            filterList =videoListVoList.stream().filter(videoListVo -> !videoListVo.getVideourl().equals("")).collect(Collectors.toList());
+        }catch (NullPointerException n){
+
+        }
+        filterList.forEach(videoListVo -> {
+            String id = userMapping.getIdByUsername(username) + "-" + videoListVo.getId();
+            try {
                 videoListVo.setIssupport(videoMapping.getSupportById(id) == 1);
-            }catch (BindingException e){
+            } catch (BindingException e) {
                 videoListVo.setIssupport(false);
             }
 
         });
 
-        return videoListVos;
+        return filterList;
     }
 
     @Override
     public boolean addVideo(String username, String title, String videourl) {
         int userid = userMapping.getIdByUsername(username);
-        Date date=new Date();
-        SimpleDateFormat dateFormat=new SimpleDateFormat("YYYY-MM-dd");
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
         String time = dateFormat.format(date);
-        return videoMapping.addVideo(userid,title,videourl,time);
+        return videoMapping.addVideo(userid, title, videourl, time);
     }
 
     @Override
     public boolean support(int videoid, String username) {
         int userid = userMapping.getIdByUsername(username);
-        String userAddVideo = userid+"-"+videoid;
-        videoMapping.support(userAddVideo,userid,videoid);
+        String userAddVideo = userid + "-" + videoid;
+        videoMapping.support(userAddVideo, userid, videoid);
         return true;
     }
 }
